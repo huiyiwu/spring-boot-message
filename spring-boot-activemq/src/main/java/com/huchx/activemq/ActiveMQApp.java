ab806783ab954f71ec6545f69191a48e26e2a335
huchx <296101967@qq.com>
2021-01-08 17:27:29 +0800

    init

---
+++ spring-boot-activemq/src/main/java/com/huchx/activemq/ActiveMQApp.java
@@ -0,0 +1,18 @@
+package com.huchx.activemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
+
+/**
+ *消息传递ActiveMQ实现
+ */
+@SpringBootApplication
+@EnableJms
+public class ActiveMQApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ActiveMQApp.class,args);
+    }
+}
---
+++ spring-boot-activemq/src/main/java/com/huchx/activemq/jms/MsgProducer.java
@@ -0,0 +1,13 @@
+package com.huchx.activemq.jms;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Author: Huchx
+ * Date: 2021/1/8 10:57
+ */
+@Service("producer")
+public class MsgProducer {
+
+}
